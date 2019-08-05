02e0d0de76934d3720b1c32bd3cfc445ed5dd80d
Cory Cardwell <>
2019-08-04 23:50:13 -0600

    emojis

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h2>&#9996;</h2>
+    <h1>Sup Cam!</h1>
+    <span style='font-size:100px;'>&#129500;&#127999;</span>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
@@ -24,5 +24,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-size: 50px;
+}
+h2 {
+  font-size: 80px;
 }
 </style>
